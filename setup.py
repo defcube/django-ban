cec41931ebf90095649bde48eaf6c69bbc90dede
Justin Quick <justquick@gmail.com>
2010-07-08 10:57:52 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
  
 setup(
     name = "django-ban",
-    version = '0.1.1',
+    version = '0.1.2',
     url = 'http://github.com/justquick/django-ban',
     author = 'Alexander Artemenko, Justin Quick',
     description = 'Simple middleware to ban users by IP or IP network ',
