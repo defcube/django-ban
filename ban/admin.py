63d58920e04d797cb265c0caa34ba1fa2a457459
Justin Quick <justquick@gmail.com>
2010-07-08 10:57:23 -0400

    searchable ip in admin

--- ban/admin.py
+++ ban/admin.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from models import DeniedIP, AllowedIP
 
+class IPAdmin(admin.ModelAdmin):
+    search_fields = ('ip',)
+
 if 'ban.middleware.DenyMiddleware' in settings.MIDDLEWARE_CLASSES:
-    admin.site.register(DeniedIP)
+    admin.site.register(DeniedIP, IPAdmin)
     
 if 'ban.middleware.AllowMiddleware' in settings.MIDDLEWARE_CLASSES:
-    admin.site.register(AllowedIP)
+    admin.site.register(AllowedIP, IPAdmin)
